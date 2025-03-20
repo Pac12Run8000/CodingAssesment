e1104a05a9ebeef94f614f239019227869763992
Norbert Grover <norbert298@gmail.com>
2025-03-19 21:29:29 -0700

    Add the XIB file for the custom cell

--- CodingAssesment/Views/ViewController.swift
+++ CodingAssesment/Views/ViewController.swift
@@ -5,29 +5,35 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView:UITableView!
-    private var viewModel: CountryViewModel!
+    
+    let countries: [Country] = [
+        Country(name: "Canada", region: "North America", capital: "Ottawa", code: "CAN"),
+        Country(name: "Germany", region: "Europe", capital: "Berlin", code: "DEU"),
+        Country(name: "Japan", region: "Asia", capital: "Tokyo", code: "JPN"),
+        Country(name: "Brazil", region: "South America", capital: "Brasília", code: "BRA")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.viewModel = CountryViewModel(dataProvider: CountryDataManager())
-        
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.reloadData()
+        
+        tableView.register(UINib(nibName: "CountryCell", bundle: nil), forCellReuseIdentifier: "CountryCell")
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return viewModel.numberOfCountries()
-        }
+            return countries.count
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath) as! CountryCell
-        let country = viewModel.country(at: indexPath.row)
-        cell.configure(with: country)  
-        print("Cell height for row \(indexPath.row): \(cell.frame.height)")
-
+        
+        // Assuming you have a Country model, configure the cell
+        let country = countries[indexPath.row]
+        cell.configure(with: country)
+        
         return cell
     }
     
