b13522de36322afbc2a1470db6d282e69f81bf13
Norbert Grover <norbert298@gmail.com>
2025-03-20 17:25:16 -0700

    I broke the URL down into components

--- CodingAssesment/Models/CountryDataManager.swift
+++ CodingAssesment/Models/CountryDataManager.swift
@@ -3,9 +3,20 @@ import Foundation
 class CountryDataManager: ProtocolDataProvider {
     static let shared = CountryDataManager() // Singleton instance
 
-    private let apiURL = "https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.json"
+    private var apiURL: URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "gist.githubusercontent.com"
+        components.path = "/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.json"
+        
+        return components.url
+    }
 
     func fetchCountries(completion: @escaping (Result<[Country], Error>) -> Void) {
-        NetworkService.shared.fetchData(from: apiURL, completion: completion)
+        guard let url = apiURL else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        NetworkService.shared.fetchData(from: url.absoluteString, completion: completion)
     }
 }
